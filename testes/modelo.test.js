40f4d5190f42c7e1c8a370779304be458dce70e8
rodrigo-sales <rodrigo.nasc2@gmail.com>
2024-06-22 21:50:12 -0300

    Adicionados novos testes para 100% de cobertura

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -23,3 +23,27 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
+
+test('Testando get de pergunta inexistente', () => {
+  const pergunta = modelo.get_pergunta(1);
+  expect(pergunta).toBeUndefined();
+});
+
+test('Testando get de respostas inexistentes', () => {
+  const respostas = modelo.get_respostas(1);
+  expect(respostas.length).toBe(0);
+});
+
+test('Testando número de respostas de uma pergunta', () => {
+  const id_pergunta = modelo.cadastrar_pergunta('5 + 5 = ?');
+  modelo.cadastrar_resposta(id_pergunta, '10');
+  modelo.cadastrar_resposta(id_pergunta, 'Não sei');
+  modelo.cadastrar_resposta(id_pergunta, 'Dez');
+  const num_respostas = modelo.get_num_respostas(id_pergunta);
+  expect(num_respostas).toBe(3);
+});
+
+
+
+
+
